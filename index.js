69fccce15d197dba5c52c4e8d8e4c6c482e6d3ca
Borrison <borissoto@gmail.com>
2021-02-01 23:47:09 -0400

    shuffle neighbor pairs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {
   // Mouse,
 } = Matter;
 
-const cells = 5;
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -36,6 +36,20 @@ const walls = [
 World.add(world, walls);
 
 // Maze generation
+
+const shuffle = (arr) => {
+  let counter = arr.length;
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+};
+
 const grid = Array(cells)
   .fill(null)
   .map(() => Array(cells).fill(false));
@@ -47,7 +61,36 @@ const horizontals = Array(cells - 1)
   .fill(null)
   .map(() => Array(cells).fill(false));
 
-console.log(horizontals);
+const startRow = Math.floor(Math.random() * cells);
+const startColumns = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  // If i have visited the cell at [row, column], then return
+  if (grid[row][column]) {
+    return;
+  }
+  // Mark this cell as being visited
+  grid[row][column] = true;
+  // Assemble randomly-ordered list of neighbors
+  const neighbors = shuffle([
+    [row - 1, column],
+    [row, column + 1],
+    [row + 1, column],
+    [row, column - 1],
+  ]);
+
+  // console.log(neighbors);
+
+  // For each neighbor...
+  // See if that neighbor is out of bounds
+  // If we have visited that heighbor, continue to next neighbor
+  // Remove a wall from either horizontals or verticals
+  // Visit that next cell
+};
+
+stepThroughCell(1, 1);
+
+// console.log(grid);
 
 // DEMO
 // World.add(
