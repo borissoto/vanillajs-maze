d5e4488d20407290d47af0cfdcee857fb68599af
Borrison <borissoto@gmail.com>
2021-02-04 23:51:40 -0400

    drawing horizontal segments

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -115,13 +117,22 @@ const stepThroughCell = (row, column) => {
 
 stepThroughCell(startRow, startColumns);
 
-horizontals.forEach((row) => {
-  row.forEach((open) => {
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
     if (open) {
       return;
     }
 
-    const wall = Bodies.rectangle();
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength / 2,
+      rowIndex * unitLength + unitLength,
+      unitLength,
+      10,
+      {
+        isStatic: true,
+      }
+    );
+    World.add(world, wall);
   });
 });
 
